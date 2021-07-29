c2d5522223197cd34affb48779c1b4f356807e17
oheunju <61354985+oheunju@users.noreply.github.com>
2021-07-29 17:51:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from indeed import extract_indeed_pages, extract_indeed_jobs
 # 최대 페이지 구하기
 last_indeed_pages = extract_indeed_pages()
 # 페이지당 일목록 가져오기
-extract_indeed_jobs(last_indeed_pages)
+indeed_jobs = extract_indeed_jobs(last_indeed_pages)
+print(indeed_jobs)
